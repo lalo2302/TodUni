71fd451b1f8f4262a6c1016f80cafb609c1fb580
Jorge Vazquez Mellado <joevzq@gmail.com>
2016-03-19 15:58:01 -0600

    generate proyectos model

--- TodUni/db/schema.rb
+++ TodUni/db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160319214141) do
+ActiveRecord::Schema.define(version: 20160319215412) do
+
+  create_table "proyectos", force: :cascade do |t|
+    t.string   "titulo",      limit: 255
+    t.text     "descripcion", limit: 65535
+    t.string   "coordenadas", limit: 255
+    t.date     "fecha_final"
+    t.integer  "estatus",     limit: 4,     default: 0
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
