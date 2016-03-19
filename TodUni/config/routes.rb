88f50bdf197c350f6e3a1b654f6454fbce64d1b5
Eduardo Aguilera <laloao.2302@gmail.com>
2016-03-19 15:57:45 -0600

    Create user controller

--- TodUni/config/routes.rb
+++ TodUni/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'home/index'
   root to: "home#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
