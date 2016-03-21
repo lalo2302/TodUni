c6e2458847923bfd309d1c9be54096b2dcd71ffa
Eduardo Aguilera <laloao.2302@gmail.com>
2016-03-21 16:40:15 -0600

    User needs to complete his info to create a project

--- TodUni/app/controllers/registrations_controller.rb
+++ TodUni/app/controllers/registrations_controller.rb
@@ -1,12 +1,20 @@
 class RegistrationsController < Devise::RegistrationsController
-  before_filter :configure_permitted_parameters, :only => [:create]
+  before_filter :configure_permitted_parameters, :only => [:create, :update]
+  
+  def after_sign_in_path_for(resource)
+    dashboard_path(current_user)
+  end
+
   def after_sign_up_path_for(resource)
     dashboard_path(current_user)
   end
 
+  
+
 protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :name, :birth_date) }
     end
 
 end
