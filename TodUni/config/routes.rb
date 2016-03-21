86ed571a35256a90bab3f4822bf180a29dbaf45d
Eduardo Aguilera <laloao.2302@gmail.com>
2016-03-21 10:50:40 -0600

    Add dashboard resources

--- TodUni/config/routes.rb
+++ TodUni/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "home#index"
+  resources :dashboards
+  devise_for :users, :controllers => { :registrations => "registrations" }
+  root "home#index"
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
