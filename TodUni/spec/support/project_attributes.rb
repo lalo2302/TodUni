6ed5b861ff8c6555faa0db6024ac4b3726effd07
Jorge Vazquez Mellado <joevzq@gmail.com>
2016-06-01 11:32:24 -0500

    Add projects_attributes support ruby class for testing

---
+++ TodUni/spec/support/project_attributes.rb
@@ -0,0 +1,11 @@
+def project_attributes(overrides = {})
+	{
+		name: "Test project",
+		description: "This projects is a test",
+		coordinates: "21.1250° N, 101.6860° W",
+		date_beginning: Date.new(2016,06,01),
+		date_end: Date.new(2016,12,31),
+		budget: 10000,
+		status: 1
+	}.merger(overrides)
+end
