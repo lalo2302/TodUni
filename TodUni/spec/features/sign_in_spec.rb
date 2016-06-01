f42d7d3dde201e876195f9e760ef0fb7b14cf022
Eduardo Aguilera <laloao.2302@gmail.com>
2016-06-01 12:02:36 -0500

    Sign in test passes

---
+++ TodUni/spec/features/sign_in_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+describe "The sigin process" do
+	#before :each do
+	#	user = FactoryGirl.create(:user)
+	#	user.save
+	#end
+
+	it "signs in" do
+		user = FactoryGirl.create(:user)
+		user.save
+		visit new_user_session_path
+		fill_in :email, :with => 'example@example.com'
+		fill_in :password, :with => '12345678'
+		
+		click_button 'Entra'	
+
+		expect(page).to have_current_path(dashboard_path(user))
+	end
+end
