daa7c3acabf68d4920ced80f43dada0e674860ef
Eduardo Aguilera <laloao.2302@gmail.com>
2016-06-01 16:36:52 -0500

    Wrong signin test

--- TodUni/spec/features/sign_in_spec.rb
+++ TodUni/spec/features/sign_in_spec.rb
@@ -16,4 +16,15 @@ describe "The sigin process" do
 
 		expect(page).to have_current_path(dashboard_path(user))
 	end
+
+	it "Doesn't sign in" do
+		visit new_user_session_path
+
+		fill_in :email, :with => 'example@example.com'
+		fill_in :password, :with => '12345678'
+		
+		click_button 'Entra'	
+
+		expect(page).to have_current_path(new_user_session_path)
+	end
 end
