80ab076c1a69450c591f6c47951ab2c6fe06306b
Eduardo Aguilera <laloao.2302@gmail.com>
2016-06-02 11:20:19 -0500

    Add project factory and removes birthdate of user

--- TodUni/spec/factories.rb
+++ TodUni/spec/factories.rb
@@ -4,6 +4,11 @@ FactoryGirl.define do
 		name "Example"
 		password "12345678"
 		password_confirmation "12345678"
-		birth_date Date.today - 20.year
 	end	
+
+	factory :project do
+		name "Project example"
+		description "Project description"
+		assosiation user
+	end
 end
