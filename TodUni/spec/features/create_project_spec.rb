883e82a74d46e22928db3d081c04586efebf5461
Eduardo Aguilera <laloao.2302@gmail.com>
2016-06-02 21:05:27 -0500

    Refactor fill of field, and uses let to declare lazy global variables

--- TodUni/spec/features/create_project_spec.rb
+++ TodUni/spec/features/create_project_spec.rb
@@ -1,52 +1,50 @@
 require 'rails_helper'
-describe "The create a project process" do
-	before :each do
-		user = FactoryGirl.create(:user)
-		login_as(user)
+describe "The create-a-project process" do
+	let(:user) { FactoryGirl.create :user }
+	let(:project) { FactoryGirl.create :project }
 
-		project = FactoryGirl.create(:project)
+	def fill_field(field, content)
+		visit new_project_path
+		fill_in field, :with => content
+		click_button "Registrar"
 	end
 
-	context "When creates creates a valid project" do
-
-		it "Redirects to project path" do
+	context "when creates a valid project" do
+		it "redirects to project path" do
+			login_as(user)
 			visit new_project_path
 
 			fill_in :name, :with => 'Nombre de proyecto'
 	  	fill_in :description, :with => 'Descripcion del proyecto'
-			
 			click_button "Registrar"
 
 			expect(page).to have_current_path(project_path(Project.last))
 		end
 	end
+	
+	context "when creates a project without name" do
+		it "stays in new project path" do
+			fill_field(:description, 'Descripcion del proyecto')
 
-	context "When creates a project without name" do
-
-		visit new_project_path
-		fill_in :description, :with => 'Descripcion de proyecto'
-		click_button "Registrar"
-
-		it "Stays in new project path" do
 			expect(page).to have_current_path(new_project_path)
 		end
 		it "shows a flash message" do
+			fill_field(:description, 'Descripcion del proyecto')
+
 			expect(page).to have_content('Dale un nombre a tu proyecto')
 		end
 	end
 
-	context "When creates a project without description" do
+	context "when creates a project without description" do
+		it "stays in new project path" do
+			fill_field(:name, 'Nombre del proyecto')
 
-		visit new_project_path
-		fill_in :name, :with => 'Nombre del projecto'
-		click_button "Registrar"
-
-		it "Stays in new project path" do
 			expect(page).to have_current_path(new_project_path)
 		end
 		it "shows a flash message" do
+			fill_field(:name, 'Nombre del proyecto')
+			
 			expect(page).to have_content('Dale una descripcion a tu proyecto')
 		end
-
 	end
 end
