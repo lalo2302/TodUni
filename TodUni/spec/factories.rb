ffd0b4934712c10109b9a2d76315200289844f97
Eduardo Aguilera <laloao.2302@gmail.com>
2016-06-03 12:24:56 -0500

    Add complete user factory

--- TodUni/spec/factories.rb
+++ TodUni/spec/factories.rb
@@ -1,14 +1,21 @@
 FactoryGirl.define do
 	factory :user do
 		email "example@example.com"
-		name "Example"
 		password "12345678"
 		password_confirmation "12345678"
 	end	
 
+	factory :user_complete, class: User do
+		email "example@example.com"
+		name "Example"
+		password "12345678"
+		password_confirmation "12345678"
+		birth_date Date.today - 20.years
+	end
+
 	factory :project do
 		name "Project example"
 		description "Project description"
-		assosiation user
+		assosiation user_complete
 	end
 end
