ceb491a81d2751fbe60068451f8cf900ef95e397
Jorge Vazquez Mellado <joevzq@gmail.com>
2016-06-04 17:30:45 -0500

    Add projects factory, edit users factory

--- TodUni/spec/factories.rb
+++ TodUni/spec/factories.rb
@@ -1,9 +1,30 @@
 FactoryGirl.define do
 	factory :user do
-		email "example@example.com"
 		name "Example"
+		email { "#{name}@example.com" }
 		password "12345678"
 		password_confirmation "12345678"
-		birth_date Date.today - 20.year
+		date_birth Date.today - 20.year
 	end	
+
+	factory :project do
+		sequence(:name) { |n| "TEST_PROJECT_#{n}" }
+		description "This project is a test"
+
+		factory :pre_project do
+			status 0
+		end
+
+		factory :approved_project do
+			status 1
+		end
+
+		factory :finished_project do
+			status 2
+		end
+
+		factory :canceled_project do
+			status 3
+		end
+	end
 end
