45597c2a66273553f86881ed96a41b4d79175cf9
Eduardo Aguilera <laloao.2302@gmail.com>
2016-06-04 13:35:18 -0500

    Refactor test to do the correct paths

--- TodUni/spec/features/create_project_spec.rb
+++ TodUni/spec/features/create_project_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 describe "The create-a-project process" do
-	let(:user) { FactoryGirl.create :user }
-	let(:project) { FactoryGirl.create :project }
+	let(:user) { FactoryGirl.create :user_complete }
 
 	def fill_field(field, content)
+		login_as(user)
 		visit new_project_path
 		fill_in field, :with => content
-		click_button "Registrar"
+		click_button "Crear proyecto"
 	end
 
 	context "when creates a valid project" do
@@ -14,37 +14,42 @@ describe "The create-a-project process" do
 			login_as(user)
 			visit new_project_path
 
-			fill_in :name, :with => 'Nombre de proyecto'
-	  	fill_in :description, :with => 'Descripcion del proyecto'
-			click_button "Registrar"
+			fill_in :name, :with => 'Nombre de proyecto' 
+		 	fill_in :description, :with => 'Descripcion del proyecto'
+			click_button "Crear proyecto"
 
 			expect(page).to have_current_path(project_path(Project.last))
 		end
 	end
 	
 	context "when creates a project without name" do
+
 		it "stays in new project path" do
 			fill_field(:description, 'Descripcion del proyecto')
-
-			expect(page).to have_current_path(new_project_path)
+			save_and_open_page
+			#TODO: Make rspec to wait until page is loaded
+			expect(page).to have_current_path(new_project_path, :wait => 1.0)
 		end
+
 		it "shows a flash message" do
 			fill_field(:description, 'Descripcion del proyecto')
 
-			expect(page).to have_content('Dale un nombre a tu proyecto')
+			expect(page).to have_content('Tu proyecto necesita nombre')
 		end
 	end
 
 	context "when creates a project without description" do
+
 		it "stays in new project path" do
 			fill_field(:name, 'Nombre del proyecto')
 
 			expect(page).to have_current_path(new_project_path)
 		end
+
 		it "shows a flash message" do
 			fill_field(:name, 'Nombre del proyecto')
 			
-			expect(page).to have_content('Dale una descripcion a tu proyecto')
+			expect(page).to have_content('Tu proyecto necesita descripcion')
 		end
 	end
 end
