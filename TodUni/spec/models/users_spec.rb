fd74ee71100c3e2b563bc3c1ce433c64ef611a9d
Jorge Vazquez Mellado <joevzq@gmail.com>
2016-06-05 19:37:36 -0500

    Edit users_spec model test

--- TodUni/spec/models/users_spec.rb
+++ TodUni/spec/models/users_spec.rb
@@ -1,27 +1,60 @@
-require 'rails_helper'
+require "rails_helper"
 
-describe "A user" do
+RSpec.describe User, :type => :model do
 
-	it "requires a email" do
-		user = User.create(email: "")
-		user.valid?
-		expect(user.errors[:email].any?).to eq(true)
+	it "has a valid factory" do
+		expect(build(:user)).to be_valid
 	end
-	it "accepts properly formatted email addresses" do
-		emails = %w[user@example.com first.last@example.com]
-		emails.each do |email|
-			user = User.create(email: email)
-			user.valid?
-			expect(user.errors[:email].any?). to eq(false)
+
+	describe "Validations" do
+			
+		it "requires a name" do
+			user = User.new(name:"")
+
+			user.valid? # populates the errors
+			expect(user.errors[:name].any?).to eq(true)
 		end
-	end
-	it "rejects improperly formatted email addresses" do
-		emails = %w[@ user@ @example.com .com]
-		emails.each do |email|
-			user = User.create(email: email)
+
+		it "requires an email" do
+			user = User.new(email:"")
+
 			user.valid?
 			expect(user.errors[:email].any?).to eq(true)
 		end
-	end
 
+		it "accepts properly formatted email address" do
+			emails = %w[user@example.com test.user@ex.c]
+			emails.each do |e|
+				user = User.new(email:e)
+
+				user.valid?
+				expect(user.errors[:email].any?).to eq(false)
+			end
+		end
+
+		it "rejects improperly formatted email address" do
+			emails = %w[u.c u@@c y@.c user@ @com example e@a@ex.com e.@com]
+			emails.each do |e|
+				user = User.new(email:e)
+
+				user.valid?
+				expect(user.errors[:email].any?).to eq(true)
+			end
+		end
+
+		it "requires a unique, case insensitive email address" do
+			user1 = User.create!(user_attributes)
+			user2 = User.new(email: user1.email.upcase)
+
+			user2.valid?
+			expect(user2.errors[:email].any?).to eq(true)
+		end
+
+		it "requires a password" do
+			user = User.new(password:"")
+
+			user.valid?
+			expect(user.errors[:password].any?).to eq(true)
+		end
+	end
 end
