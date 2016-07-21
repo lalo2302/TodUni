7c6e6693ce7dc3099cd9cc3f4a7cbd7c3b7814e4
Jorge Vazquez Mellado <joevzq@gmail.com>
2016-07-21 14:43:43 -0500

    Set locale from current signed in user if available

--- TodUni/app/controllers/application_controller.rb
+++ TodUni/app/controllers/application_controller.rb
@@ -5,13 +5,21 @@ class ApplicationController < ActionController::Base
 	before_action :set_i18n_locale_from_params
 
   def after_sign_in_path_for(resource)
+		if current_user.is_a?(User) && current_user.locale != I18n.locale
+			I18n.locale = current_user.locale
+		end
+
     dashboard_path(current_user)
   end
 
 	private
 
 		def set_i18n_locale_from_params
-			if params[:locale]
+			valid_locales = ['en', 'es']
+
+			if user_signed_in? && valid_locales.include?(current_user.locale)
+				I18n.locale = current_user.locale
+			elsif params[:locale]
 				if I18n.available_locales.map(&:to_s).include?(params[:locale])
 					I18n.locale = params[:locale]
 				else
@@ -24,5 +32,4 @@ class ApplicationController < ActionController::Base
 		def default_url_options(options = {})
 			{locale: I18n.locale}
 		end
-
 end
