6f57bb940ed833afec3cddd4c2092ebdd93576ea
Eduardo Aguilera <laloao.2302@gmail.com>
2016-09-04 16:19:30 -0500

    Tests added to show a project

--- TodUni/spec/features/projects/visit_project_spec.rb
+++ TodUni/spec/features/projects/visit_project_spec.rb
@@ -9,12 +9,29 @@ describe "Visiting another user project process" do
     visit project_path(project) 
   end
 
+  context "the visitor" do
+    it "sees the owner name" do
+    end
+
+    it "sees the project name" do
+    end
+
+    it "sees the members' list" do
+    end
+
+    it "sees the project description" do
+    end
+  end
+
   context "when encourages an incomplete project" do
     it "encourages 1 time" do
     end
 
     it "can't encourage 2 times" do
     end 
+
+    it "the phases are not shown" do
+    end
   end
 
   context "when asks to be a project's member" do
