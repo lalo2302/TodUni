cb612ddac57943b6d190783d41d4b619468783a1
Eduardo Aguilera <laloao.2302@gmail.com>
2016-09-04 16:43:02 -0500

    Add description to tests

--- TodUni/spec/features/projects/visit_preproject_spec.rb
+++ TodUni/spec/features/projects/visit_preproject_spec.rb
@@ -11,26 +11,58 @@ describe "Visiting another user project process" do
 
   context "the visitor" do
     it "sees the owner name" do
+      got_to_project
+
+      expect(page).to have_content(owner.name)
+    end
+
+    it "identifies the owner" do
+      go_to_project
+
+      expect(page).to have_xpath('//li[@id="owner"]')
     end
 
     it "sees the project name" do
-    end
+      go_to_project
 
-    it "sees the members' list" do
+      expect(page).to have_content(project.name)
     end
 
     it "sees the project description" do
+      go_to_project
+
+      expect(page).to have_content(project.description)
+    end
+
+    it "sees the members' list" do
+      go_to_project
+
+      expect(page).to have_xpath('//ul[@id="members"]')
+    end
+
+    it "sees the project description" do
+      go_to_project
+
+      expect(page).to have_content(project.description)
     end
   end
 
   context "when encourages an incomplete project" do
     it "encourages 1 time" do
+      go_to_project
+
+      click_button("¡Vamos!")
+
+      expect(page).to have_content("Tú crees en este proyecto")
     end
 
     it "can't encourage 2 times" do
-    end 
+      go_to_project
 
-    it "the phases are not shown" do
+      click_button("¡Vamos!")
+
+      expect(page).not_to have_button("¡Vamos!")
+    end 
     end
   end
 
